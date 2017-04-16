31a56246603e8bbfce6e0f48e48aaa6d888925b1
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2017-04-16 09:38:52 -0700

    committed the udacity file

--- behavior_cloning/udacity.py
+++ behavior_cloning/udacity.py
@@ -1,4 +1,4 @@
-from driverless import *
+from model import *
 
 def display_change_brightness_graph(images, steering_angles):
     plt.subplots(figsize=(20, 5))
@@ -24,38 +24,68 @@ def display_flipped_image(images, steering_angles):
     plt.show()
 
 def display_original_image():
-    df = pd.read_csv('data/track1_1/driving_log.csv')
+    cols = ['center_image', 'left_image', 'right_image', 'steering_angle', 'throttle', 'brake', 'speed']
+    df = pd.read_csv('data/track1_1/driving_log.csv', names=cols)
     # Get 3 rows of data and display them
-    np.random.random()
+    print(len(df))
+    idx = np.random.randint(1000, size=3)
+    print(idx)
+    ndf = df.ix[idx]
+
+    fig, axs = plt.subplots(3, 3, figsize=(5, 5))
+    i = 0
+    for idx, row in ndf.iterrows():
+        axs[i][0].imshow(read_image(row['left_image']))
+        axs[i][1].imshow(read_image(row['center_image']))
+        axs[i][2].imshow(read_image(row['right_image']))
+
+        steering_angle = float(row['steering_angle'])
+        axs[i][0].set_title("Left Image Angle: {:.4f}".format(steering_angle + 0.25))
+        axs[i][1].set_title("Center Image Angle: {:.4f}".format(steering_angle))
+        axs[i][2].set_title("Right Image Angle: {:.4f}".format(steering_angle - 0.25))
+
+        axs[i][0].axis('off')
+        axs[i][1].axis('off')
+        axs[i][2].axis('off')
+
+        axs[i][0].set_axis_bgcolor('gray')
+        axs[i][1].set_axis_bgcolor('gray')
+        axs[i][2].set_axis_bgcolor('gray')
+        i += 1
+
+    fig.tight_layout()
+    plt.show()
+
 
 def main():
     df = read_training_data("track1")
     df = rearrange_and_augment_dataframe(df, shuffle_data=True)
 
     # Get the first 10 images
-    df_first_10 = df[1:11]
-
-    images = []
-    angles = []
-    for i, row in df_first_10.iterrows():
-        img = read_image(row.image)
-        steering_angle = row.steering_angle
-        img = change_brightness(img)
-        images.append(img)
-        angles.append(steering_angle)
-
-    # display_change_brightness_graph(images, angles)
-
-    df_10 = df[20:25]
-    images = []
-    angles = []
-    for i, row in df_10.iterrows():
-        img = read_image(row.image)
-        steering_angle = row.steering_angle
-        images.append(img)
-        angles.append(steering_angle)
-
-    display_flipped_image(images, angles)
+    # df_first_10 = df[1:11]
+    #
+    # images = []
+    # angles = []
+    # for i, row in df_first_10.iterrows():
+    #     img = read_image(row.image)
+    #     steering_angle = row.steering_angle
+    #     img = change_brightness(img)
+    #     images.append(img)
+    #     angles.append(steering_angle)
+    #
+    # # display_change_brightness_graph(images, angles)
+    #
+    # df_10 = df[20:25]
+    # images = []
+    # angles = []
+    # for i, row in df_10.iterrows():
+    #     img = read_image(row.image)
+    #     steering_angle = row.steering_angle
+    #     images.append(img)
+    #     angles.append(steering_angle)
+    #
+    # display_flipped_image(images, angles)
+    display_original_image()
 
 if __name__ == "__main__":
     main()
